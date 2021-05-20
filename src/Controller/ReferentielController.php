30917e6720f32f09bc742f6fcee8e0bf79a9695d
GaelH <g.herambourg.gh@gmail.com>
2021-05-20 17:02:25 +0200

    Gestion js du clique sur le referentiel souhaite
    Gestion des boutons modifier selon ligne selectionnee ou non

--- src/Controller/ReferentielController.php
+++ src/Controller/ReferentielController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ReferentielRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +14,16 @@ class ReferentielController extends AbstractController
     /**
      * @Route("/referentiel/liste", name="referentiel_liste")
      */
-    public function listerReferentiel()
+    public function listerReferentiel(Request $request,
+                                      EntityManagerInterface $entitymanager,
+                                      ReferentielRepository $referentielRepository)
     {
+        $tous_les_referentiels = $referentielRepository->findAll();
+
+        dump($tous_les_referentiels);
+
         return $this->render('referentiel/referentiel_liste.html.twig', [
+            'tous_les_referentiels' => $tous_les_referentiels
         ]);
     }
 }
