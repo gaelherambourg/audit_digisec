e3ca812c33167f5e5522b94ff32fefe4e604d00b
GaelH <g.herambourg.gh@gmail.com>
2021-05-21 16:18:44 +0200

    Fonction Recherche : Référentiels - Partie 1

--- src/Controller/ReferentielController.php
+++ src/Controller/ReferentielController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\RechercheSimpleType;
 use App\Repository\ReferentielRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,11 +19,37 @@ class ReferentielController extends AbstractController
                                       EntityManagerInterface $entitymanager,
                                       ReferentielRepository $referentielRepository)
     {
+        //Création du formulaire de recherche
+        $form = $this->createForm(RechercheSimpleType::class);
+        $form->handleRequest($request);
+
+        $referentiel_recherches = "";
+        $recherche_utilisateur = "";
+
         //On récupère tous les référentiels en bdd
         $tous_les_referentiels = $referentielRepository->findAll();
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $recherche_utilisateur = $form->get('recherche')->getData();
+
+            $referentiel_recherches =$referentielRepository->recherche($recherche_utilisateur);
+            return $this->render('referentiel/referentiel_liste.html.twig',[
+                'tous_les_referentiels' => $tous_les_referentiels,
+                'baseVide'=> 'Aucun référentiel ne correspond à votre recherche.',
+                'referentiel_recherches'=> $referentiel_recherches,
+                'recherche_utilisateur' => $recherche_utilisateur,
+                'form_recherche_referentiel' => $form->createView()
+            ]);
+        }
+
+        
+
         return $this->render('referentiel/referentiel_liste.html.twig', [
-            'tous_les_referentiels' => $tous_les_referentiels
+            'tous_les_referentiels' => $tous_les_referentiels,
+            'referentiel_recherches'=> $referentiel_recherches,
+            'recherche_utilisateur' => $recherche_utilisateur,
+            'form_recherche_referentiel' => $form->createView()
         ]);
     }
 }
